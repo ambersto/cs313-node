b97bbccce3128864b2ac3916bfaf57f03bb7c35e
ambersto <sto12021@byui.edu>
2018-07-07 23:46:18 -0600

    Completed web service endpoints. Need to fix functionality

--- server.js
+++ server.js
@@ -9,10 +9,44 @@ const pool = new Pool({connectionString: connectionString});
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-// Returns list of all events
+
+app.get('/getUserId', function(request, response) {
+	getUserId(request, response);
+});
+app.post('/addUser', function(request, response) {
+	addUser(request, response);
+});
+app.post('/addVenue', function(request, response) {
+	addVenue(request, response);
+});
+app.post('/addEvent', function(request, response) {
+	addEvent(request, response);
+});
 app.get('/getEvents', function(request, response) {
 	getEvents(request, response);
 });
+app.get('/getDay', function(request, response) {
+	getDay(request, response);
+});
+app.get('/getEventDetails', function(request, response) {
+	getEventDetails(request, response);
+});
+app.get('/getVenueDetails', function(request, response) {
+	getVenueDetails(request, response);
+});
+app.get('/getVenues', function(request, response) {
+	getVenues(request, response);
+});
+app.get('/getUserDetails', function(request, response) {
+	getUserDetails(request, response);
+});
+app.patch('/updateVenue', function(request, response) {
+	updateVenue(request, response);
+});
+app.patch('/updateEvent', function(request, response) {
+	updateEvent(request, response);
+});
+
 
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
@@ -27,7 +61,13 @@ function getUserId(request, response) {
 	var firstname = request.query.firstname;
 	var lastname = request.query.lastname;
 	console.log("Retrieving user id");
-	getUserIdFromDB(firstname, lastname, requestCallback)	
+	getUserIdFromDB(firstname, lastname, function (error, result) {
+	if(error || result == null) {
+		response.status(500).json({success: false, data: error});
+	} else {
+		response.status(200).json(result);
+	}
+});
 }
 
 // add a new user
@@ -35,7 +75,13 @@ function addUser(request, response) {
 	var firstname = request.query.firstname;
 	var lastname = request.query.lastname;
 	console.log("Adding new user");
-	addUserToDB(firstname, lastname, requestCallback);
+	addUserToDB(firstname, lastname, function (error, result) {
+	if(error || result == null) {
+		response.status(500).json({success: false, data: error});
+	} else {
+		response.status(200).json(result);
+	}
+});
 }
 
 // add a new venue
@@ -48,7 +94,13 @@ function addVenue(request, response) {
 	var phone = request.query.phone;
 	var email = request.query.email;
 	console.log("Adding new venue");
-	addVenueToDB(venueName, street, city, state, zip, phone, email, requestCallback);
+	addVenueToDB(venueName, street, city, state, zip, phone, email, function (error, result) {
+	if(error || result == null) {
+		response.status(500).json({success: false, data: error});
+	} else {
+		response.status(200).json(result);
+	}
+});
 }
 
 // add a new event
@@ -59,13 +111,25 @@ function addEvent(request, response) {
 	var authorId = request.query.authorId;
 	var notes = request.query.notes;
 	console.log("Adding new event");
-	addEventToDB(eventName, eventDate, venueId, authorId, notes, requestCallback);
+	addEventToDB(eventName, eventDate, venueId, authorId, notes, function (error, result) {
+	if(error || result == null) {
+		response.status(500).json({success: false, data: error});
+	} else {
+		response.status(200).json(result);
+	}
+});
 }
 
 // get list of all events
 function getEvents(request, response) {
 	console.log("Retrieving all events");
-	getEventsFromDB(requestCallback);
+	getEventsFromDB(function (error, result) {
+	if(error || result == null) {
+		response.status(500).json({success: false, data: error});
+	} else {
+		response.status(200).json(result);
+	}
+});
 }
 
 // get events from specified day
@@ -73,38 +137,68 @@ function getDay(request, response) {
 	var dayStart = request.query.dayStart;
 	var dayEnd = request.query.dayEnd;
 	console.log("Retrieving event for specified day");
-	getEventsFromDB(dayStart, dayEnd, requestCallback);
+	getEventsFromDB(dayStart, dayEnd, function (error, result) {
+	if(error || result == null) {
+		response.status(500).json({success: false, data: error});
+	} else {
+		response.status(200).json(result);
+	}
+});
 }
 
 // get event details
 function getEventDetails(request, response) {
 	var eventId = request.query.eventId;
 	console.log("Retrieving details for event");
-	getEventDetails(eventId, requestCallback);
+	getEventDetails(eventId, function (error, result) {
+	if(error || result == null) {
+		response.status(500).json({success: false, data: error});
+	} else {
+		response.status(200).json(result);
+	}
+});
 }
 
 // get venue details
 function getVenueDetails(request, response) {
 	var venueId = request.query.venueId;
 	console.log("Retrieving details for venue");
-	getEventDetails(venueId, requestCallback);
+	getEventDetails(venueId, function (error, result) {
+	if(error || result == null) {
+		response.status(500).json({success: false, data: error});
+	} else {
+		response.status(200).json(result);
+	}
+});
 }
 
 // get list of all venues
 function getVenues(request, response) {
 	console.log("Retrieving all events");
-	getVenuesFromDB(requestCallback);
+	getVenuesFromDB(function (error, result) {
+	if(error || result == null) {
+		response.status(500).json({success: false, data: error});
+	} else {
+		response.status(200).json(result);
+	}
+});
 }
 
 // get user details
 function getUserDetails(request, response) {
 	var userId = request.query.userId;
 	console.log("Retrieving details for user");
-	getEventDetails(userId, requestCallback);
+	getUserDetailsFromDB(userId, function (error, result) {
+	if(error || result == null) {
+		response.status(500).json({success: false, data: error});
+	} else {
+		response.status(200).json(result);
+	}
+});
 }
 
 // update a venue
-function updateVenue(request, response) {\
+function updateVenue(request, response) {
 	var venueId = request.query.venueId;
 	var venueName = request.query.venueName;
 	var street = request.query.street;
@@ -114,7 +208,13 @@ function updateVenue(request, response) {\
 	var phone = request.query.phone;
 	var email = request.query.email;
 	console.log("Updating venue");
-	updateVenueInDB(venueId, venueName, street, city, state, zip, phone, email, requestCallback);
+	updateVenueInDB(venueId, venueName, street, city, state, zip, phone, email, function (error, result) {
+	if(error || result == null) {
+		response.status(500).json({success: false, data: error});
+	} else {
+		response.status(200).json(result);
+	}
+});
 }
 
 // update an event
@@ -126,18 +226,13 @@ function updateEvent(request, response) {
 	var authorId = request.query.authorId;
 	var notes = request.query.notes;
 	console.log("Updating event");
-	updateEventInDB(eventId, eventName, eventDate, venueId, authorId, notes, requestCallback);
-}
-
-/********************************************************
- * Callback function for requests
- *******************************************************/
-function requestCallback(error, result) {
+	updateEventInDB(eventId, eventName, eventDate, venueId, authorId, notes, function (error, result) {
 	if(error || result == null) {
 		response.status(500).json({success: false, data: error});
 	} else {
 		response.status(200).json(result);
 	}
+});
 }
 
 /********************************************************
@@ -148,7 +243,15 @@ function getUserIdFromDB(firstname, lastname, callback) {
 	console.log("Getting user ID from DB");
 	var sql = "SELECT id FROM author WHERE first_name = $1::VARCHAR AND last_name = $2::VARCHAR";
 	var params = [firstname, lastname];
-	pool.query(sql, params, selectCallback);
+	pool.query(sql, params, function(err, result) {
+	if (err) {
+		console.log("Error in query: ");
+		console.log(err);
+		callback(err, null);
+	}
+	console.log("Found result: " + JSON.stringify(result.rows));
+	callback(null, result.rows);
+});
 }
 
 // insert new user
@@ -156,93 +259,7 @@ function addUserToDB(firstname, lastname, callback) {
 	console.log("Adding user to DB");
 	var sql = "INSERT INTO author (first_name, last_name) VALUES ($1::VARCHAR, $2::VARCHAR)";
 	var params = [firstname, lastname];
-	pool.query(sql, params, insertCallback);
-}
-
-// insert new venue
-function addVenueToDB(venueName, street, city, state, zip, phone, email, callback) {
-	console.log("Adding venue to DB");
-	var sql = "INSERT INTO venue (venue_name, street, city, state, zip, phone, email) VALUES ($1::VARCHAR, $2::VARCHAR, $3::VARCHAR, $4::VARCHAR, $5::INT, $6::VARCHAR, $7::VARCHAR)";
-	var params = [venueName, street, city, state, zip, phone, email];
-	pool.query(sql, params, insertCallback);
-}
-
-// insert new event
-function addEventToDB(eventName, eventDate, venueId, authorId, notes, callback) {
-	console.log("Adding event to DB");
-	var sql = "INSERT INTO event (event_name, event_date, venue_id, author_id, notes) VALUES ($1::VARCHAR, $2::TIMESTAMP, $3::INT, $4::INT, $5::TEXT)";
-	var params = [eventName, eventDate, venueId, authorId, notes];
-	pool.query(sql, params, insertCallback);
-}
-
-// get list of all events
-function getEventsFromDB(callback) {
-	console.log("Getting events from DB");
-	var sql = "SELECT e.id, e.event_name, e.event_date, v.venue_name, a.first_name, e.notes FROM event e INNER JOIN venue v ON e.venue_id=v.id INNER JOIN author a ON e.author_id=a.id";
-	pool.query(sql, selectCallback);
-}
-
-// get events for a specific day
-function getDayFromDB(dayStart, dayEnd, callback) {
-	console.log("Getting events for a specific day from DB");
-	var sql = "SELECT e.id, e.event_name, e.event_date, v.venue_name, a.first_name, e.notes FROM event e INNER JOIN venue v ON e.venue_id=v.id INNER JOIN author a ON e.author_id=a.id WHERE e.event_date = $1::TIMESTAMP";
-	var params = [dayStart, dayEnd];
-	pool.query(sql, params, selectCallback);
-}
-
-// get details for an event
-function getEventDetailsFromDB(eventId, callback) {
-	console.log("Getting details for an event from DB");
-	var sql = "SELECT e.id, e.event_name, e.event_date, v.venue_name, a.first_name, e.notes FROM event e INNER JOIN venue v ON e.venue_id=v.id INNER JOIN author a ON e.author_id=a.id WHERE e.id = $1::INT";
-	var params = [eventId];
-	pool.query(sql, params, selectCallback);
-}
-
-// get details for a venue
-function getVenueDetailsFromDB(venueId, callback) {
-	console.log("Getting details for a venue from DB");
-	var sql = "SELECT * FROM venue WHERE id = $1::INT";
-	var params = [venueId];
-	pool.query(sql, params, selectCallback);
-}
-
-// get list of all venues
-function getVenuesFromDB(callback) {
-	console.log("Getting venues from DB");
-	var sql = "SELECT id, venue_name FROM venue";
-	pool.query(sql, selectCallback);
-}
-
-// get details for a user
-function getUserDetailsFromDB(userId, callback) {
-	console.log("Getting details for a user from DB");
-	var sql = "SELECT * FROM author WHERE id = $1::INT";
-	var params = [userId];
-	pool.query(sql, params, selectCallback);
-}
-
-// update a venue
-function updateVenueInDB(venueId, venueName, street, city, state, zip, phone, email, callback) {
-	console.log("Updating venue in DB");
-	var sql = "UPDATE venue SET venue_name = $1::VARCHAR, street = $2::VARCHAR, city = $3::VARCHAR, state = $4::VARCHAR, zip = $5::INT, phone = $6::VARCHAR, email = $7::VARCHAR WHERE id = $8::INT";
-	var params = [venueName, street, city, state, zip, phone, email, venueId];
-	pool.query(sql, params, updateCallback);
-}
-
-// update an event
-function updateEventInDB(eventId, eventName, eventDate, venueId, authorId, notes, callback) {
-	console.log("Updating event in DB");
-	var sql = "UPDATE event SET event_name = $1::VARCHAR, event_date = $2::TIMESTAMP, venue_id = $3::INT, author_id = $4::INT, notes = $5::TEXT WHERE id = $6::INT";
-	var params = [eventName, eventDate, venueId, authorId, notes, eventId];
-	pool.query(sql, params, updateCallback);
-}
-
-
-/****************************************************
- * Callback functions for DB queries
- ***************************************************/
-// Callback function for all inserts
-function insertCallback(err, result) {
+	pool.query(sql, params, function(err, result) {
 	if (err) {
 		console.log("Error in query: ");
 		console.log(err);
@@ -251,10 +268,48 @@ function insertCallback(err, result) {
 	// TODO: test this line
 	console.log("Inserted row with ID:  " + JSON.stringify(result.insertId));
 	callback(null, result.rows);
+});
 }
 
-// Callback function for all select queries
-function selectCallback(err, result) {
+// insert new venue
+function addVenueToDB(venueName, street, city, state, zip, phone, email, callback) {
+	console.log("Adding venue to DB");
+	var sql = "INSERT INTO venue (venue_name, street, city, state, zip, phone, email) VALUES ($1::VARCHAR, $2::VARCHAR, $3::VARCHAR, $4::VARCHAR, $5::INT, $6::VARCHAR, $7::VARCHAR)";
+	var params = [venueName, street, city, state, zip, phone, email];
+	pool.query(sql, params, function(err, result) {
+	if (err) {
+		console.log("Error in query: ");
+		console.log(err);
+		callback(err, null);
+	}
+	// TODO: test this line
+	console.log("Inserted row with ID:  " + JSON.stringify(result.insertId));
+	callback(null, result.rows);
+});
+}
+
+// insert new event
+function addEventToDB(eventName, eventDate, venueId, authorId, notes, callback) {
+	console.log("Adding event to DB");
+	var sql = "INSERT INTO event (event_name, event_date, venue_id, author_id, notes) VALUES ($1::VARCHAR, $2::TIMESTAMP, $3::INT, $4::INT, $5::TEXT)";
+	var params = [eventName, eventDate, venueId, authorId, notes];
+	pool.query(sql, params, function(err, result) {
+	if (err) {
+		console.log("Error in query: ");
+		console.log(err);
+		callback(err, null);
+	}
+	// TODO: test this line
+	console.log("Inserted row with ID:  " + JSON.stringify(result.insertId));
+	callback(null, result.rows);
+});
+}
+
+// get list of all events
+function getEventsFromDB(callback) {
+	console.log("Getting events from DB");
+	var sql = "SELECT e.id, e.event_name, e.event_date, v.venue_name, a.first_name, e.notes FROM event e INNER JOIN venue v ON e.venue_id=v.id INNER JOIN author a ON e.author_id=a.id";
+	pool.query(sql, function(err, result) {
 	if (err) {
 		console.log("Error in query: ");
 		console.log(err);
@@ -262,10 +317,94 @@ function selectCallback(err, result) {
 	}
 	console.log("Found result: " + JSON.stringify(result.rows));
 	callback(null, result.rows);
+});
 }
 
-// Callback function for all updates
-function updateCallback(err, result) {
+// get events for a specific day
+function getDayFromDB(dayStart, dayEnd, callback) {
+	console.log("Getting events for a specific day from DB");
+	var sql = "SELECT e.id, e.event_name, e.event_date, v.venue_name, a.first_name, e.notes FROM event e INNER JOIN venue v ON e.venue_id=v.id INNER JOIN author a ON e.author_id=a.id WHERE e.event_date = $1::TIMESTAMP";
+	var params = [dayStart, dayEnd];
+	pool.query(sql, params, function(err, result) {
+	if (err) {
+		console.log("Error in query: ");
+		console.log(err);
+		callback(err, null);
+	}
+	console.log("Found result: " + JSON.stringify(result.rows));
+	callback(null, result.rows);
+});
+}
+
+// get details for an event
+function getEventDetailsFromDB(eventId, callback) {
+	console.log("Getting details for an event from DB");
+	var sql = "SELECT e.id, e.event_name, e.event_date, v.venue_name, a.first_name, e.notes FROM event e INNER JOIN venue v ON e.venue_id=v.id INNER JOIN author a ON e.author_id=a.id WHERE e.id = $1::INT";
+	var params = [eventId];
+	pool.query(sql, params, function(err, result) {
+	if (err) {
+		console.log("Error in query: ");
+		console.log(err);
+		callback(err, null);
+	}
+	console.log("Found result: " + JSON.stringify(result.rows));
+	callback(null, result.rows);
+});
+}
+
+// get details for a venue
+function getVenueDetailsFromDB(venueId, callback) {
+	console.log("Getting details for a venue from DB");
+	var sql = "SELECT * FROM venue WHERE id = $1::INT";
+	var params = [venueId];
+	pool.query(sql, params, function(err, result) {
+	if (err) {
+		console.log("Error in query: ");
+		console.log(err);
+		callback(err, null);
+	}
+	console.log("Found result: " + JSON.stringify(result.rows));
+	callback(null, result.rows);
+});
+}
+
+// get list of all venues
+function getVenuesFromDB(callback) {
+	console.log("Getting venues from DB");
+	var sql = "SELECT id, venue_name FROM venue";
+	pool.query(sql, function(err, result) {
+	if (err) {
+		console.log("Error in query: ");
+		console.log(err);
+		callback(err, null);
+	}
+	console.log("Found result: " + JSON.stringify(result.rows));
+	callback(null, result.rows);
+});
+}
+
+// get details for a user
+function getUserDetailsFromDB(userId, callback) {
+	console.log("Getting details for a user from DB");
+	var sql = "SELECT * FROM author WHERE id = $1::INT";
+	var params = [userId];
+	pool.query(sql, params, function(err, result) {
+	if (err) {
+		console.log("Error in query: ");
+		console.log(err);
+		callback(err, null);
+	}
+	console.log("Found result: " + JSON.stringify(result.rows));
+	callback(null, result.rows);
+});
+}
+
+// update a venue
+function updateVenueInDB(venueId, venueName, street, city, state, zip, phone, email, callback) {
+	console.log("Updating venue in DB");
+	var sql = "UPDATE venue SET venue_name = $1::VARCHAR, street = $2::VARCHAR, city = $3::VARCHAR, state = $4::VARCHAR, zip = $5::INT, phone = $6::VARCHAR, email = $7::VARCHAR WHERE id = $8::INT";
+	var params = [venueName, street, city, state, zip, phone, email, venueId];
+	pool.query(sql, params, function (err, result) {
 	if (err) {
 		console.log("Error in query: ");
 		console.log(err);
@@ -274,4 +413,22 @@ function updateCallback(err, result) {
 	// TODO: test this line
 	console.log("Updated row with ID:  " + JSON.stringify(result.insertId));
 	callback(null, result.rows);
+});
+}
+
+// update an event
+function updateEventInDB(eventId, eventName, eventDate, venueId, authorId, notes, callback) {
+	console.log("Updating event in DB");
+	var sql = "UPDATE event SET event_name = $1::VARCHAR, event_date = $2::TIMESTAMP, venue_id = $3::INT, author_id = $4::INT, notes = $5::TEXT WHERE id = $6::INT";
+	var params = [eventName, eventDate, venueId, authorId, notes, eventId];
+	pool.query(sql, params, function (err, result) {
+	if (err) {
+		console.log("Error in query: ");
+		console.log(err);
+		callback(err, null);
+	}
+	// TODO: test this line
+	console.log("Updated row with ID:  " + JSON.stringify(result.insertId));
+	callback(null, result.rows);
+});
 }
