a0d5340c3c5096829194d1bb276f6b76e5aa6f12
ambersto <sto12021@byui.edu>
2018-07-12 16:33:28 -0600

    Trying to fix disappearing issue

--- public/eventscheduler.js
+++ public/eventscheduler.js
@@ -18,3 +18,6 @@ function login(){
 
 // TODO: page only shows result briefly, then it disappears... why?
 // TODO: result is displayed as {object Object} even though query is correctly returning author id
+// TODO: autopopulate list of venues onLoad -> https://www.w3schools.com/tags/ev_onload.asp
+//       https://www.w3schools.com/tags/tag_select.asp
+//       https://stackoverflow.com/questions/8418811/is-there-a-way-to-use-javascript-to-populate-a-html-dropdown-menu-with-values
