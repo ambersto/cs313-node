cdbb67130ef43d443fdca7ecf81d8eb4c2e6180f
ambersto <sto12021@byui.edu>
2018-07-11 21:15:37 -0600

    adding html and frontend js

--- public/eventscheduler.js
+++ public/eventscheduler.js
@@ -1,17 +1,17 @@
 function login(){
-	var firstname = $('#firstname').val();
-	var lastname = $('#lastname').val();
+	var firstname = $("#firstname").val();
+	var lastname = $("#lastname").val();
 	
 	var params = {
 		firstname: firstname,
 		lastname: lastname
 	};
 
-	$.get('/getUserId', params, function(result) {
+	$.get("/getUserId", params, function(result) {
 		if (result) {
-			$('#events').text('User id is: ' + result);
+			$("#events").text("User id is: " + result);
 		} else {
-			$('#events').text('Error logging in');
+			$("#events").text("Error logging in");
 		}
 	});
 }
