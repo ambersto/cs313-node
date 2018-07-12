bc3e5943dcdd9a753d1065ec80f96cd4bf16ee74
ambersto <sto12021@byui.edu>
2018-07-11 19:31:24 -0600

    attempting to add database stuff

--- server.js
+++ server.js
@@ -241,7 +241,7 @@ function updateEvent(request, response) {
 // get user id
 function getUserIdFromDB(firstname, lastname, callback) {
 	console.log("Getting user ID from DB");
-	var sql = "SELECT id FROM author WHERE first_name = $1::VARCHAR AND last_name = $2::VARCHAR";
+	var sql = "SELECT * FROM author WHERE first_name = $1::VARCHAR AND last_name = $2::VARCHAR";
 	var params = [firstname, lastname];
 	pool.query(sql, params, function(err, result) {
 	if (err) {
