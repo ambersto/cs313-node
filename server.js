6743693d0f9384fd19e309846a1ededa9fa20690
ambersto <sto12021@byui.edu>
2018-07-13 17:29:07 -0600

    working on add user

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const { Pool } = require("pg");
 const connectionString = process.env.DATABASE_URL || "postgres://eventuser:di2sd3kfnl@localhost:5432/eventscheduler";
 const pool = new Pool({connectionString: connectionString});
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
