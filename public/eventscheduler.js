eb9d24dc5991a6ebf08370e8f10a9b55ad54163c
ambersto <sto12021@byui.edu>
2018-07-13 15:28:06 -0600

    Adding venue list dropdown

--- public/eventscheduler.js
+++ public/eventscheduler.js
@@ -1,9 +1,11 @@
 function loadVenueList(){
 	$.get("/getVenues", function(result) {
 		if (result) {
-
+			for (i in result){
+				$("#venueList").append("<option value=\"" + result[i].id + "\">" + result[i].name + "</option>");
+			}
 		} else {
-
+			$("#venueList").append("<option>No venues available</option>")
 		}
 	});
 }
