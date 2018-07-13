f08d8e73e5143528da8e35f2a4bc7737f4a66cd5
ambersto <sto12021@byui.edu>
2018-07-13 16:38:41 -0600

    Adding venue list dropdown

--- public/eventscheduler.js
+++ public/eventscheduler.js
@@ -29,12 +29,14 @@ function login(){
 	});
 }
 
-// TODO: autopopulate list of venues onLoad -> https://www.w3schools.com/tags/ev_onload.asp
-//       https://www.w3schools.com/tags/tag_select.asp
-//       https://stackoverflow.com/questions/8418811/is-there-a-way-to-use-javascript-to-populate-a-html-dropdown-menu-with-values
+
 
 /*******************************************
  * Completed tasks
  ******************************************/
 // TODO: page only shows result briefly, then it disappears... why?
 // TODO: result is displayed as {object Object} even though query is correctly returning author id
+// TODO: autopopulate list of venues onLoad -> https://www.w3schools.com/tags/ev_onload.asp
+//       https://www.w3schools.com/tags/tag_select.asp
+//       https://stackoverflow.com/questions/8418811/is-there-a-way-to-use-javascript-to-populate-a-html-dropdown-menu-with-values
+//		 https://stackoverflow.com/questions/10578619/jquery-dynamically-create-select-tag/10579053
