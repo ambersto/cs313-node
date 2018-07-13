f418ead9d5bfb1e924c5bc4ec708f1797ffc973b
ambersto <sto12021@byui.edu>
2018-07-13 14:14:44 -0600

    Trying to display user id

--- public/eventscheduler.js
+++ public/eventscheduler.js
@@ -1,3 +1,13 @@
+function loadVenueList(){
+	$.get("/getVenues", function(result) {
+		if (result) {
+
+		} else {
+
+		}
+	});
+}
+
 function login(){
 	var firstname = $("#firstname").val();
 	var lastname = $("#lastname").val();
@@ -9,15 +19,19 @@ function login(){
 
 	$.get("/getUserId", params, function(result) {
 		if (result) {
-			$("#events").text("User id is: " + JSON.stringify(result));
+			$("#events").text("User id is: " + JSON.stringify(result.rows[0].id));
 		} else {
 			$("#events").text("Error logging in");
 		}
 	});
 }
 
-// TODO: page only shows result briefly, then it disappears... why?
-// TODO: result is displayed as {object Object} even though query is correctly returning author id
 // TODO: autopopulate list of venues onLoad -> https://www.w3schools.com/tags/ev_onload.asp
 //       https://www.w3schools.com/tags/tag_select.asp
 //       https://stackoverflow.com/questions/8418811/is-there-a-way-to-use-javascript-to-populate-a-html-dropdown-menu-with-values
+
+/*******************************************
+ * Completed tasks
+ ******************************************/
+// TODO: page only shows result briefly, then it disappears... why?
+// TODO: result is displayed as {object Object} even though query is correctly returning author id
