c351be04491798a933f1207cb6525472817607fa
ambersto <sto12021@byui.edu>
2018-07-17 13:38:11 -0600

    working on displaying events

--- public/eventscheduler.js
+++ public/eventscheduler.js
@@ -173,7 +173,7 @@ function loadEventList() {
 			console.log("Showing events");
 			$("#eventList").empty();
 			for (i in result){
-				$("#eventList").append("<li value=\"" + JSON.stringify(result[i].id) + "\">" + JSON.stringify(result[i].event_name) + "</li>");
+				$("#eventList").append("<li name=\"" + JSON.stringify(result[i].id) + "\">" + JSON.stringify(result[i].event_name) + "</li>");
 			}
 		} else {
 			console.log("Error loading events");
