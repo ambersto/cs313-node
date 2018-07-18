987e995287e69622ea6948b6d0df5aab6b303a28
ambersto <sto12021@byui.edu>
2018-07-17 20:09:18 -0600

    adding select date function

--- public/eventscheduler.js
+++ public/eventscheduler.js
@@ -178,7 +178,7 @@ function showEvents() {
 		if (result) {	
 			console.log("Showing events");
 			$("#eventList").empty();
-			$("#eventList").append("<li>" + selectedDate + "</li>");
+			$("#eventList").append("<li>" + selectedDate.getFullYear() + "</li>");
 			for (i in result){
 				var newDate = stringToDate(result[i].event_date);
 				$("#eventList").append("<li value=\"" + result[i].id + "\">" + getHoursAndMinutes(newDate) + " - " + result[i].event_name + "</li>");
